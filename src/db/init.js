003b67a6532010e45ce244380306c3a8a5d12a9d
ranieljs97 <ranieljs97@gmail.com>
2022-03-27 23:26:17 -0300

    add:  room integration db

--- src/db/init.js
+++ src/db/init.js
@@ -7,13 +7,14 @@ const initDb = {
     await db.exec(`CREATE TABLE rooms (
       id INTEGER PRIMARY KEY,
       pass TEXT 
-    )`);
+    )`)
 
     await db.exec(`CREATE TABLE questions ( 
       id INTEGER PRIMARY KEY AUTOINCREMENT,  
-      titulo TEXT,
-      read INT
-    )`);
+      title TEXT,
+      read INT,
+      room INT
+    )`)
 
     await db.close()
   }
