4bb13f44b0f4ad6975a2c677256fbb168aa7d8e8
ranieljs97 <ranieljs97@gmail.com>
2022-03-28 23:59:42 -0300

    add: actions to questions

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -40,16 +40,16 @@ module.exports = {
     const db = await Database()
     const roomId = req.params.room
     const questions = await db.all(
-      `SELECT * FROM questions WHERE room = ${roomId}`
+      `SELECT * FROM questions WHERE room = ${roomId} and read=0 `
     )
-    const questionRead = await db.all(
+    const questionsRead = await db.all(
       `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
     )
 
     res.render('room', {
       roomId: roomId,
       questions: questions,
-      questionRead: questionRead
+      questionsRead: questionsRead
     })
   }
 }
